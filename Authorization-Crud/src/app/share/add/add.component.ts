62483c7104fe84211290ded4aa2c04068b743fe7
DmitrijFirst <levchenkodima9992@gmail.com>
2020-02-03 22:14:08 +0200

    Создаем форму для отправки ведомости

--- Authorization-Crud/src/app/share/add/add.component.ts
+++ Authorization-Crud/src/app/share/add/add.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, FormBuilder, FormArray } from "@angular/forms";
 
 @Component({
   selector: 'app-add',
@@ -7,9 +8,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddComponent implements OnInit {
 
-  constructor() { }
+  personsForm: FormGroup;
+  inputValue = "";
+  constructor(private fb: FormBuilder) {}
+
+
+
+  onFormSubmit() {
+    console.log(this.personsForm.value);
+  }
+
+addPerson(){
+  const users = this.personsForm.get('persons') as FormArray;
+  users.push(this.fb.group({
+    name: [''],
+    surname: [''],
+    pers_num: [''],
+    length_of_work: [''],
+    status: [''],
+    phone: [''],
+    salary: ['']
+  }))
+}
+
+  get persons() {
+    return (<FormArray>this.personsForm.get("persons")).controls;
+  }
+
+  removePerson(i) {
+    (<FormArray>this.personsForm.get("persons")).removeAt(i);
+  }
 
   ngOnInit() {
+    this.personsForm = this.fb.group({
+      id: new FormControl(''),
+      title: new FormControl(''),
+      persons: this.fb.array([])
+  });
   }
 
 }
